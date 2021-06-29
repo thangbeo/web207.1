ad2177ff5eb1943d163015ac428e156b4db52046
thangbeo <thangbeo1004@gmail.com>
2021-06-29 13:32:32 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function tong(a, b) {
+  return a + b;
+}
+const a = 20;
+const b = 20;
+
+const data = `${(<div>total:{tong(a, b)}</div>)}`;
+
+ReactDOM.render(data, document.getElementById("root"));
+
+// serviceWorker.unregister();
